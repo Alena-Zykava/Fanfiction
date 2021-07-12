599818d7788c15d1088127430b67a862b02b9145
Alena-Zykava <zykava.alena@gmail.com>
2021-07-12 11:46:23 +0300

    fix loader userPage

--- src/components/Pages/UserFanficsPage/UserFanficsPage.tsx
+++ src/components/Pages/UserFanficsPage/UserFanficsPage.tsx
@@ -3,10 +3,14 @@ import { deleteFanfic } from '../../../utilities/service';
 import FanficItem from '../../FanficItem';
 import { IFanfic } from '../../../models/Fanfic';
 import { useGetUserFanfics } from '../../../hooks/useGetUserFanfics';
+import { RootState } from '../../../models/Interfaces';
+import { useSelector } from 'react-redux';
+import Loader from '../../Loader';
 
 const UserFanficsPage = () => {
 
     const { dataUserFanfics, setDataUserFanfics } = useGetUserFanfics();
+    const {isFetching} = useSelector((state: RootState)=> state.fanfics)
 
     const handlerClick = (id: any) => {
         deleteFanfic(id).then((res) => {
@@ -16,6 +20,10 @@ const UserFanficsPage = () => {
 
     return (
         <>
+        {isFetching ?
+            <Loader />
+            : (
+            <>
             {dataUserFanfics.sort((a: IFanfic, b: IFanfic) => {       
                     return a.lastDataUpdate > b.lastDataUpdate ? -1 : 1;
                 }).map((fanfic: IFanfic) => {
@@ -25,6 +33,8 @@ const UserFanficsPage = () => {
                         fanfic={fanfic} />)
             })}
         </>
+        )}
+        </>    
     )
 }
 
